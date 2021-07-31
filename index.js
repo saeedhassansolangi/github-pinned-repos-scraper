1eb78980b5f5e5c544d71b51fb56a3c9d5cc3ed5
saeedhassansolangi <saeedhassansolangi@gmail.com>
2021-07-31 22:50:32 +0500

    installing cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const ScrapingGithub = require('./scraping-pinned-repos');
 
 const app = express();
 
+app.use(cors());
 const PORT = process.env.PORT || 3000;
-console.log(PORT);
 
 app.get('/', async (req, res) => {
   const githubData = await ScrapingGithub();
